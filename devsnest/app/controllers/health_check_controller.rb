328f2ba004f0b0f8d16ca3e19b5d47470bfe6dee
Vedansh Garg <vedanshkt.sfc@gmail.com>
2021-02-26 00:01:39 +0530

    add health check endpoint

---
+++ devsnest/app/controllers/health_check_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class HealthCheckController < ApplicationController
+  def index
+    render_success
+  end
+end
--- devsnest/config/routes.rb
+++ devsnest/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/health_check', to: 'health_check#index'
   namespace :api do
     namespace :v1 do
       devise_for :users,
