4932ea21d43a7c6732c3f033ecfe932b35a40f23
Vedansh Garg <vedanshkt.sfc@gmail.com>
2021-02-26 18:43:39 +0530

    once more changing domain

--- devsnest/config/initializers/session_store.rb
+++ devsnest/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Devsnest::Application.config.session_store :cookie_store, :key => '_interslice_session', :domain => ".devsnest.in"
+Devsnest::Application.config.session_store :cookie_store, :key => '_interslice_session', :domain => "devsnest.in"
