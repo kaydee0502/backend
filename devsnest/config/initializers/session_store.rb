e3e791dc2a0290117d424a2a724e09c31ce53ea4
Vedansh Garg <vedanshkt.sfc@gmail.com>
2021-02-27 08:34:13 +0530

    work, work, work this time 2

--- devsnest/config/initializers/session_store.rb
+++ devsnest/config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_interslice_session', domain: :all, tld_length: 2
-Rails.application.config.session_store :cookie_store, key: '_devsnest_session', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_interslice_session', domain: '.devsnest.in', tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_devsnest_session', domain: '.devsnest.in', tld_length: 2
