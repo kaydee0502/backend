0d94f1a7305fd2b07662f8ddd2410ca75c6ca07b
kaydee0502 <kshitijdhama513@gmail.com>
2021-05-28 13:21:11 +0530

    🗃 added activity attribute, after social links.
    🗃  college reverse migration
    
    ⚡️ Arrayfied solved and total questions
    
    Replaced arrays with hashmap
    
    Added registration_num to users
    
    Added Edit functionality for User

--- devsnest/app/resources/api/v1/user_resource.rb
+++ devsnest/app/resources/api/v1/user_resource.rb
@@ -4,17 +4,22 @@ module Api
   module V1
     class UserResource < JSONAPI::Resource
       attributes :email, :name, :password, :discord_id, :web_active, :username, :score, :discord_active, :batch, :grad_status, :grad_specialization, :grad_year,
-                 :github_url, :linkedin_url, :resume_url, :dob
+                 :github_url, :linkedin_url, :resume_url, :dob, :registration_num, :college_id
       attributes :group_id, :group_name 
-      attributes :college
-      attributes :easy_solved, :medium_solved, :hard_solved
-
+      attributes :college_name
+      attributes :solved, :total_by_difficulty
+      attributes :activity
+      
       def fetchable_fields
         if context[:user].nil? || context[:user].id == @model.id
-          super - [:password]
+          super - [:password, :college_id]
         else
-          super - [:password, :email]
+          super - [:password, :email, :college_id]
         end
+      end
+
+      def self.updatable_fields(context)
+        super - [:score, :group_id, :group_name]
       end
 
       def group_id
@@ -31,27 +36,35 @@ module Api
         group.present? ? group.name : nil
       end
 
-      def college
+      def college_name
         return nil if context[:user].nil?
-        member = College.where(user_id: context[:user].id).first       
-        member.present? ? member.name : nil
+        return nil if context[:user].college_id.nil?
+        cid = context[:user].college_id      
+        cid.present? ? College.where(id: cid).first.name : nil
       end
 
-      def easy_solved
+      def solved
         return nil if context[:user].nil?
-        Submission.count_solved(context[:user].id, "easy")
+        Submission.count_solved(context[:user].id)
       end   
 
-      def medium_solved
+      def total_by_difficulty
         return nil if context[:user].nil?
-        Submission.count_solved(context[:user].id, "medium")
-      end  
-
-      def hard_solved
+        Content.split_by_difficulty
+      end
+      
+      def activity
         return nil if context[:user].nil?
-        Submission.count_solved(context[:user].id, "hard")
-      end  
-
+        dates = Hash.new
+        Submission.where(status:"done",user_id:context[:user].id).or(Submission.where(status:"doubt",user_id:context[:user].id)).all.each do |user|
+          if dates.key?(user.updated_at.to_date)
+            dates[user.updated_at.to_date] += 1
+          else
+            dates[user.updated_at.to_date] = 1
+          end
+        end
+        return dates
+      end
     end
   end
 end
