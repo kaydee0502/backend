5e97b3bb0d35cb8a89243e2dad896e80427779d3
kaydee0502 <kshitijdhama513@gmail.com>
2021-06-07 15:17:21 +0530

    ♿️ added slug fields

--- devsnest/app/controllers/api/v1/groups_controller.rb
+++ devsnest/app/controllers/api/v1/groups_controller.rb
@@ -5,8 +5,8 @@ module Api
     class GroupsController < ApplicationController
       include JSONAPI::ActsAsResourceController
       before_action :simple_auth
+      before_action :deslug, only: %i[show]
       before_action :check_authorization, only: %i[show]
-      
 
       def context
         { user: @current_user }
@@ -17,18 +17,29 @@ module Api
         return render_not_found unless group.present?
         if !(group.group_members.where(user_id: @current_user.id).present? || group.batch_leader_id == @current_user.id || @current_user.user_type == 2)
           return render_forbidden
-        end   
+        end
       end
 
-      def index        
+      def deslug
+        slug_name = params[:id]
+        group = Group.find_by(slug: slug_name)
+        return render_not_found unless group.present?
+        params[:id] = group.id
+      end
+
+      def index
         if @current_user.user_type == 0
-          @groups = Group.find_by(id: params[:id])          
+          @groups = Group.find_by(id: params[:id])
         elsif @current_user.user_type == 1
           @groups = Group.where(batch_leader_id: @current_user.id)
         elsif @current_user.user_type == 2
-          @groups = Group.all          
+          @groups = Group.all 
         end
-        return render :json => @groups
+        
+        response = { :data => @groups, :my_group => GroupMember.find_by(user_id: @current_user.id).group_id }
+
+        render :json => response
+
       end
     end
   end
--- devsnest/app/resources/api/v1/group_resource.rb
+++ devsnest/app/resources/api/v1/group_resource.rb
@@ -3,8 +3,7 @@
 module Api
   module V1
     class GroupResource < JSONAPI::Resource
-      attributes :name, :owner_id, :co_owner_id, :members_count, :student_mentor_id, :slug
-      attributes :tl_names
+      attributes :name, :owner_id, :co_owner_id, :members_count, :student_mentor_id
       has_many :group_members     
     end    
   end
