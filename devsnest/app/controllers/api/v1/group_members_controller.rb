1aa94f83e1969bf37d744c0e7762acdd5d0e9a2d
patheticGeek <geekpathetic@gmail.com>
2021-06-14 14:42:25 +0530

    ✨ More things for yug to clean up - Added update user group route - Merge group when merginf temp user - Added delete group route
    Co-authored-by: kaydee0502 <kshitijdhama513@gmail.com>

--- devsnest/app/controllers/api/v1/group_members_controller.rb
+++ devsnest/app/controllers/api/v1/group_members_controller.rb
@@ -5,7 +5,8 @@ module Api
     class GroupMembersController < ApplicationController
       include JSONAPI::ActsAsResourceController
       before_action :simple_auth
-      before_action :check_authorization
+      before_action :check_authorization, except: %i[update_user_group]
+      before_action :bot_auth, only: %i[update_user_group]
 
       def context
         { user: @current_user }
@@ -16,6 +17,47 @@ module Api
         return render_not_found unless group.present?
 
         return render_forbidden unless group.check_auth(@current_user)
+      end
+
+      def update_user_group
+        discord_id = params['data']['attributes']['discord_id']
+        updated_group_name = params['data']['attributes']['updated_group_name']
+        is_team_leader = params['data']['attributes']['is_team_leader']
+        is_vice_team_leader = params['data']['attributes']['is_vice_team_leader']
+
+        super if discord_id.nil?
+
+        user = User.find_by(discord_id: discord_id)
+        return render_error('User not found') if user.nil?
+
+        current_group_member = GroupMember.find_by(user_id: user.id)
+        current_group = Group.find_by(id: current_group_member.group_id) if current_group_member.present?
+
+        if updated_group_name.present?
+          # update the user's group
+          new_group = Group.find_by(name: updated_group_name)
+          # create group if not already exists
+          new_group = Group.create(name: updated_group_name) if new_group.nil?
+
+          if current_group_member.present?
+            # user is member of a group just need to update it
+            current_group_member.update(group_id: new_group.id)
+          else
+            # user was not part of any group need to create member
+            GroupMember.create(user_id: user.id, group_id: new_group.id)
+          end
+
+          if is_team_leader
+            # is team leader of his group
+            Group.update(owner_id: user.id) if user.id != new_group.owner_id
+          elsif is_vice_team_leader
+            # is vice team leader of his group
+            Group.update(co_owner_id: user.id) if user.id != new_group.co_owner_id
+          end
+        else
+          # delete the user from group
+          current_group_member.destroy
+        end
       end
     end
   end
