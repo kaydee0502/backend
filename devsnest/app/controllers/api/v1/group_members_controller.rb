2d45126b20ace29f029bd3252912c13c743c3e0e
patheticGeek <geekpathetic@gmail.com>
2021-06-15 10:58:01 +0530

    ♻️ Refactor update_user_group

--- devsnest/app/controllers/api/v1/group_members_controller.rb
+++ devsnest/app/controllers/api/v1/group_members_controller.rb
@@ -25,8 +25,6 @@ module Api
         is_team_leader = params['data']['attributes']['is_team_leader']
         is_vice_team_leader = params['data']['attributes']['is_vice_team_leader']
 
-        super if discord_id.nil?
-
         user = User.find_by(discord_id: discord_id)
         return render_error('User not found') if user.nil?
 
@@ -34,28 +32,27 @@ module Api
         current_group = Group.find_by(id: current_group_member.group_id) if current_group_member.present?
 
         if updated_group_name.present?
-          # update the user's group
-          new_group = Group.find_by(name: updated_group_name)
           # create group if not already exists
-          new_group = Group.create(name: updated_group_name) if new_group.nil?
+          new_group = new_group.nil? ? Group.find_by(name: updated_group_name) : Group.create(name: updated_group_name)
+
+          # remove TL and VTL from old team
+          Group.where(owner_id: user.id).update_all(owner_id: nil)
+          Group.where(co_owner_id: user.id).update_all(co_owner_id: nil)
 
           if current_group_member.present?
             # user is member of a group just need to update it
             current_group_member.update(group_id: new_group.id)
           else
             # user was not part of any group need to create member
-            GroupMember.create(user_id: user.id, group_id: new_group.id)
+            current_group_member = GroupMember.create(user_id: user.id, group_id: new_group.id)
           end
 
-          if is_team_leader
-            # is team leader of his group
-            Group.update(owner_id: user.id) if user.id != new_group.owner_id
-          elsif is_vice_team_leader
-            # is vice team leader of his group
-            Group.update(co_owner_id: user.id) if user.id != new_group.co_owner_id
-          end
+          # update TL status
+          current_group.update(owner_id: user.id) if is_team_leader
+          # update VTL status
+          current_group.update(co_owner_id: user.id) if is_vice_team_leader
         elsif current_group_member.present?
-          # delete the user from group
+          # User has been removed from a team
           current_group_member.destroy
         end
 
