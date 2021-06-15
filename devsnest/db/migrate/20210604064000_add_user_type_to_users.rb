85cb74a714f6ec21147824d8c7380258cd9c4f0d
Yug Gurnani <66687700+sam0000-cmd@users.noreply.github.com>
2021-06-16 03:34:13 +0530

    🐛 Migration bug solved (#72)
    * 🐛 Migration bug solved
    
    * 🐛 Migration bug solved

--- devsnest/db/migrate/20210604064000_add_user_type_to_users.rb
+++ devsnest/db/migrate/20210604064000_add_user_type_to_users.rb
@@ -1,5 +1,5 @@
 class AddUserTypeToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users  , :user_type, :integer,default: 0
+    add_column :users, :user_type, :integer,default: 0
   end
 end
