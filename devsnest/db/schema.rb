b9709062976b8ef7775ecc27111fd2b36e0904c4
kaydee0502 <kshitijdhama513@gmail.com>
2021-06-17 19:05:34 +0530

    restore gemfile

--- devsnest/db/schema.rb
+++ devsnest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_051811) do
+ActiveRecord::Schema.define(version: 2021_06_15_134815) do
 
   create_table "batches", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.integer "owner_id"
